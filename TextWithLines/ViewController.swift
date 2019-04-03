9bc5456108a9c48ba7c84a156b9b075f3d329f1c
DanielHJA <daniel.hjartstrom@gmail.com>
2019-04-03 08:24:12 +0200

    Initial Commit

---
+++ TextWithLines/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextWithLines
+//
+//  Created by Daniel Hjärtström on 2019-04-03.
+//  Copyright © 2019 Sog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
